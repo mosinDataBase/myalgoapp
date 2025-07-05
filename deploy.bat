da262c6b615277ad07fc6b3bd5c9069e0e6eb851
rakishop <rakishop133@gmail.com>
2025-07-06 01:02:23 +0530

    first commit

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,11 @@
 @echo off
 echo "# MyAlgo" >> README.md
-git init
+REM  git init
 git add README.md
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/mosin11/MyAlgo.git
+REM  git remote add origin https://github.com/mosin11/MyAlgo.git
 git push -u origin main
 
 echo === Deploying the app ===
