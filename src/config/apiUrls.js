96d7d99fd3bd7b626f96c8e00577e6ab88a2e178
mosinDataBase <rakishop133@gmail.com>
2025-07-20 17:21:45 +0530

    main indexs added

--- src/config/apiUrls.js
+++ src/config/apiUrls.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:10000'; 
-//const BASE_URL = 'https://myalgoserver-n87u.onrender.com'; 
+//const BASE_URL = 'http://localhost:10000'; 
+const BASE_URL = 'https://myalgoserver-n87u.onrender.com'; 
 
 const URLS = {
   socketBase      : BASE_URL,
